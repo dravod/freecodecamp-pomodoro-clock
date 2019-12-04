dee8dbd6cd77ace196215759c34b9de6eebdb491
Justin Pinero <justinpinero@gmail.com>
2019-12-04 13:52:51 -0500

    bare bones time built

--- src/components/AppClock/index.js
+++ src/components/AppClock/index.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactCountdownClock from "react-countdown-clock";
 
 export default class AppClock extends Component {
     state = {  }
     render() { 
         return (
-            <div/>
+            <div>
+                <ReactCountdownClock seconds={200}
+                     color="#000"
+                     alpha={0.9}
+                     size={300}
+                     timeFormat={"hms"}
+                     />
+            </div>
 
           );
     }
