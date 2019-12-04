349bef45b04087d8e633daa83ddafe2c54c40779
Santos Gonzalez <sgonzo3@gmail.com>
2019-12-04 13:32:51 -0500

    feat: break and session components

--- src/App.js
+++ src/App.js
@@ -11,11 +11,45 @@ function App() {
         </h1>
       </header>
       <main>
+        <Break>
+          <label id="break-label">
+            <span>"Break Length"</span>
+          </label>
+          <button id="break-decrement"></button>
+          <button id="break-increment"></button>
+        </Break>
+
+        <Session>
+          <label id="session-label">
+            <span>"Session Length"</span>
+          </label>
+          <button id="session-decrement"></button>
+          <button id="session-increment"></button>
+        </Session>
       </main>
       <footer>
       </footer>
     </div>
   );
 }
+
+class Break extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+
+     };
+   }
+}
+
+class Session extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+
+     };
+   }
+}
+
 
 export default App;
