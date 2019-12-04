bcb779c9acea6fce34fd35d08ec3a02bd41eb154
Justin Pinero <justinpinero@gmail.com>
2019-12-04 14:23:56 -0500

    building Login page

---
+++ src/containers/LoginPage/LoginHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginHeader= ()=>{
+  return(
+    <div>
+      <h1 className="Login-header">
+        
+      </h1>
+    </div>
+  )
+}
+
+export default LoginHeader
